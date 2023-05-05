ed05396e1ebb78d98083f5b1f251d535d58b9ddf
Joe W <whobbes22@gmail.com>
2023-05-05 10:03:43 -0700

    passed second test

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -4,9 +4,10 @@ namespace Bakery.Models
 {
   public class Bread
   {
+    public int Amount{get;set;}
     public Bread(int amount,int cost, int discount)
     {
-
+      Amount = amount;
     }
   }
 }
