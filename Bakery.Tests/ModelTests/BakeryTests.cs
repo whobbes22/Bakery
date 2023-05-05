5a25c01c031a0d56b4982bc0dc3a0d91c2c39270
Joe W <whobbes22@gmail.com>
2023-05-05 15:17:19 -0700

    7th test dictionary is up and running

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -47,12 +47,26 @@ namespace Bakery.Tests
     public void FindDiscountedCostPastry_ReturnActualCost_int()
     {
       int amount = 4;
-      Pastry pastry = new Pastry(2,4);
+      Pastry pastry = new Pastry(2,4,"pat");
       int dP = pastry.FindDiscountedPrice(amount);
       Assert.AreEqual(dP,6);
-      
+
     }
-    
+
+    [TestMethod]
+    public void ReportDictionary_ReturnDictionary_Dictionary()
+    {
+      Pastry pastry = new Pastry(2,4,"Pastry");
+      Dictionary<string,int[]> Dict = new Dictionary<string, int[]>() 
+        {
+          {
+          "Pastry", new int[] {0,2,4}
+          }
+        };
+      Dictionary<string,int[]> ActualDict = Pastry.GetAll();
+      Assert.AreEqual(ActualDict["Pastry"][0],Dict["Pastry"][0]);
+      
+    }    
   }
   
 }
